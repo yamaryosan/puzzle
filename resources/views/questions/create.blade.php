3bdf4b8a3261bfd8e7f3017bc9c0381efc55c106
yamaryosan <canda3267@gmail.com>
2023-10-01 00:26:25 +0900

    [add] 問題作成ビュー骨子実装

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<form action="{{route('questions.store')}}" method="post" enctype="multipart/form-data">
+    @csrf
+    <!-- エラーセクション -->
+    @include('partials.error_section', ['errors' => $errors])
+    <!-- タイトルセクション -->
+    @include('partials.question_create.title_section')
+    <!-- 問題文セクション -->
+    @include('partials.question_create.content_section')
+    <!-- 問題文用画像セクション -->
+    @include('partials.question_create.question_image_section')
+    <!-- ジャンルセクション -->
+    @include('partials.question_create.genre_section', ['existing_genres' => $existing_genres])
+    <!-- ヒントセクション -->
+    @include('partials.question_create.hint_section')
+    <!-- パターンセクション -->
+    @include('partials.question_create.pattern_section', ['patterns' => $patterns])
+    <!-- 正答セクション -->
+    @include('partials.question_create.answer_section')
+    <button type="submit">投稿</button>
+</form>
+@endsection
