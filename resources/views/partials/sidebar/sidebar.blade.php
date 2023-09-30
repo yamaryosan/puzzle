09ac8e6277622a36b20dae1c22577cae5be4cbd6
yamaryosan <canda3267@gmail.com>
2023-10-01 00:09:39 +0900

    [update] パターン機能いったん削除

--- resources/views/partials/sidebar/sidebar.blade.php
+++ resources/views/partials/sidebar/sidebar.blade.php
@@ -1,6 +1,4 @@
 <ul>
     <li><a href="{{ route('questions.index') }}">問題一覧</a></li>
     <li><a href="{{ route('questions.create') }}">問題追加</a></li>
-    <li><a href="{{ route('patterns.index') }}">パターン一覧</a></li>
-    <li><a href="{{ route('patterns.create') }}">パターン追加</a></li>
 </ul>
