73e6921e209c363584e33e6bc792d73031e59a53
yamaryosan <canda3267@gmail.com>
2023-10-01 00:06:06 +0900

    [add] resourcefulなインデックスメソッドを追加

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -9,11 +9,60 @@ use App\Models\Question;
 class QuestionsController extends Controller
 {
     /**
-     * インデックスページ
+     * インデックス
      */
     public function index(Request $request)
     {
         $questions = Question::paginate(10);
         return view('questions.index', compact('questions'));
+    }
+
+    /**
+     * 問題作成ページ用
+     */
+    public function create()
+    {
+        return view('questions.create');
+    }
+
+    /**
+     * 問題作成処理用
+     */
+    public function store()
+    {
+        return redirect(route('questions.index'));
+    }
+
+    /**
+     * 問題詳細ページ用
+     * @param string $id
+     */
+    public function show(string $id)
+    {
+
+    }
+
+    /**
+     * 問題編集ページ用
+     */
+    public function edit(string $id)
+    {
+
+    }
+
+    /**
+     * 問題編集処理用
+     */
+    public function update(string $id)
+    {
+
+    }
+
+    /**
+     * 問題削除処理用
+     */
+    public function destroy(string $id)
+    {
+
     }
 }
