ca837c1c6290ee60c13c190d761de97b7e63320c
yamaryosan <canda3267@gmail.com>
2023-09-30 23:50:53 +0900

    [add] 共通レイアウト追加

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your App Name</title>
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <!-- ここに各ページの内容が入る -->
+            @yield('content')
+        </div>
+        <!-- サイドバーの共通部分をここに含める -->
+        <div class="sidebar">
+            @include('partials.sidebar.sidebar')  <!-- サイドバーの共通部分をここに含めます -->
+        </div>
+    </div>
+</body>
+</html>
