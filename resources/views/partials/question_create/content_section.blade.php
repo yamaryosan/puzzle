9ce2e41d98c0a5559a949673ff7924b5691c56b0
yamaryosan <canda3267@gmail.com>
2023-10-01 20:01:23 +0900

    [fix] contentの入力内容が復活しないバグを改善

--- resources/views/partials/question_create/content_section.blade.php
+++ resources/views/partials/question_create/content_section.blade.php
@@ -1,4 +1,4 @@
 <div id="content_container">
     <p>内容</p>
-    <textarea name="content" placeholder="問題文を入力" rows="5" cols="20" value="{{ old('content') }}"></textarea>
+    <textarea name="content" placeholder="問題文を入力" rows="5" cols="20">{{ old('content') }}</textarea>
 </div>
