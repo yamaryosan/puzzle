ce2880533d8909e557518e8bce5cf3617bc99a0f
yamaryosan <canda3267@gmail.com>
2023-10-01 20:00:08 +0900

    [fix] 以前の入力内容を復活

--- resources/views/partials/question_create/title_section.blade.php
+++ resources/views/partials/question_create/title_section.blade.php
@@ -1,4 +1,4 @@
 <div id="title_container">
     <p>タイトル</p>
-    <input type="text" name="title" placeholder="問題のタイトルを入力" required>
+    <input type="text" name="title" placeholder="問題のタイトルを入力" value="{{old('title')}}" required>
 </div>
