48a6c9c6d7e8fc0e8a1797246a9728fa50050fb9
yamaryosan <canda3267@gmail.com>
2023-10-01 13:27:34 +0900

    [add] store用メソッド作成

---
+++ app/Services/PatternService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Question;
+use App\Models\Pattern;
+
+use App\Services\ImageService;
+
+class PatternService
+{
+    protected $imageService;
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
+
+    /**
+     * パターンタイトルを保存する
+     * @param Pattern $pattern
+     * @param string $title
+     * @return void
+     */
+    public function saveTitle($pattern, string $title)
+    {
+        $pattern->title = $title;
+    }
+
+    /**
+     * パターン本文を保存する
+     * @param Pattern $pattern
+     * @param string $content
+     * @return void
+     */
+    public function saveContent($pattern, string $content)
+    {
+        $pattern->content = $content;
+    }
+
+    public function store($form)
+    {
+        $pattern = new Pattern;
+        // タイトルと本文をDBに保存
+        $this->saveTitle($pattern, $form['title']);
+        $this->saveContent($pattern, $form['content']);
+        $pattern->save();
+        // 画像をDBに保存
+        $this->imageService->uploadForPattern($pattern, $form['pattern_images'] ?? []);
+    }
+}
