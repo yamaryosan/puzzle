2287dad7c70852229dffe3864da33c4b53555871
yamaryosan <canda3267@gmail.com>
2023-10-01 18:41:57 +0900

    [add] 使用JSファイルを記載

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+            'resources/css/app.css',
+            'resources/js/app.js',
+            'resources/js/answer_handler.js',
+            'resources/js/genre_handler.js',
+            'resources/js/hint_handler.js',
+            'resources/js/delete_all_alert.js',
+            'resources/js/hint_recoverer.js',
+            'resources/js/answer_recoverer.js',
+            'resources/js/image_drop_form_handler.js',
+            'resources/js/image_delete.js',
+            'resources/js/hint_delete.js',
+            'resources/js/answer_delete.js',
+            'resources/js/edit_page_hint_handler.js',
+            'resources/js/edit_page_answer_handler.js',
+            ],
             refresh: true,
         }),
     ],
