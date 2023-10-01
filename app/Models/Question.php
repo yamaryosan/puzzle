d39eaf7116a492feac109dbd9ec4d33a4ea75acf
yamaryosan <canda3267@gmail.com>
2023-10-01 13:32:07 +0900

    [add] ジャンル、パターン、画像の取得用メソッド

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    /**
+     * 問題からジャンルを取得
+     */
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'genre_question');
+    }
+
+    /**
+     * 問題からパターンを取得
+     */
+     public function patterns()
+    {
+        return $this->belongsToMany(Pattern::class, 'pattern_question');
+    }
+
+    /**
+     * 問題から画像を取得
+     */
+    public function images()
+    {
+        return $this->belongsToMany(Image::class, 'image_question');
+    }
 }
