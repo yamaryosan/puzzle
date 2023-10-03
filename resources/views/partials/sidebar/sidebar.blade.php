3662afebebfdc0322a8078d1d794b3ac659239f6
yamaryosan <canda3267@gmail.com>
2023-10-03 23:29:27 +0900

    [add] 検索機能サイドバー追加

--- resources/views/partials/sidebar/sidebar.blade.php
+++ resources/views/partials/sidebar/sidebar.blade.php
@@ -8,6 +8,11 @@
 <!-- ジャンル一覧 -->
 @include('partials.sidebar.genre_list')
 
+<!-- 検索フォーム -->
+<form id="search_form" action="{{ route('questions.index') }}" method="GET">
+    <input type="text" id="search_window" name="search_word" placeholder="問題を検索...">
+    <button type="submit">検索</button>
+</form>
 
 <!-- データ全削除 -->
 @include('partials.sidebar.delete_all')
