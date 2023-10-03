e4d0a204e94d02ccb2be82473d2933f4136d65c3
yamaryosan <canda3267@gmail.com>
2023-10-03 23:30:50 +0900

    [add] 検索に対応した結果表示

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
     <h1>問題一覧</h1>
-    @include('partials.question_index.result_section')
-    {{ $questions->links() }} <!-- ページネーションのリンク -->
+    @isset($searchWord)
+        <p>検索ワード：{{ $searchWord }} の検索結果 {{ $questions->total() }}件</p>
+        @empty($questions->count())
+            <p>一致する問題はありませんでした。</p>
+        @endempty
+        @include('partials.question_index.result_section', ['searchWord' => $searchWord])
+    @else
+        @empty($questions->count())
+            <p>問題がありません。最初の問題を<a href="{{ route('questions.create')}}">ここ</a>から作ってみましょう。</p>
+        @endempty
+        @include('partials.question_index.result_section', ['searchWord' => ''])
+    @endif
+    {{ $questions->links() }}
 @endsection
