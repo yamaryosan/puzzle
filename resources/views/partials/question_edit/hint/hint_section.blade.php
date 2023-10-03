f06010826e6de71fabbb4e4cee201f1eae8a1bc9
yamaryosan <canda3267@gmail.com>
2023-10-03 22:28:52 +0900

    [fix] csrfの重複を削除

--- resources/views/partials/question_edit/hint/hint_section.blade.php
+++ resources/views/partials/question_edit/hint/hint_section.blade.php
@@ -1,7 +1,8 @@
-<div id="hint_container">
-    @csrf
+<div id="existing_hint_container">
     <!-- 既存のヒント -->
     @include('partials.question_edit.hint.existing_hint_section')
+</div>
+<div id="new_hint_container">
     <!-- 新規のヒント(JSで以下の入力欄を増減) -->
 </div>
 <button type="button" id="add_hint">+</button>
