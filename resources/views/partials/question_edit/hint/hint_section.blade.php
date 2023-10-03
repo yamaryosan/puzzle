2fc50be4f4a0deb5c2e6fd0c3dcce18014cfb488
yamaryosan <canda3267@gmail.com>
2023-10-03 22:35:59 +0900

    [update] ヒント全体のコンテナを追加

--- resources/views/partials/question_edit/hint/hint_section.blade.php
+++ resources/views/partials/question_edit/hint/hint_section.blade.php
@@ -1,9 +1,12 @@
-<div id="existing_hint_container">
-    <!-- 既存のヒント -->
-    @include('partials.question_edit.hint.existing_hint_section')
-</div>
-<div id="new_hint_container">
-    <!-- 新規のヒント(JSで以下の入力欄を増減) -->
+<div id="hint_container">
+    <div id="existing_hint_container">
+        <!-- 既存のヒント -->
+        @include('partials.question_edit.hint.existing_hint_section')
+    </div>
+    <div id="new_hint_container">
+        <p>新規ヒント</p>
+        <!-- 新規のヒント(JSで以下の入力欄を増減) -->
+    </div>
 </div>
 <button type="button" id="add_hint">+</button>
 <button type="button" id="remove_hint">-</button>
